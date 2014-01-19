9373a6c99e89b08d1b5dba4181d1c22824dc6843
Romer Ramos <rramos@rramos4Geeks.(none)>
2014-01-19 14:44:12 -0430

    form agrega

--- ps/ps/views.py
+++ ps/ps/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from ps.forms import PublicacionForm
+from comercio.models import Publicacion
 
 def add(request):
     form = PublicacionForm()
@@ -11,8 +12,11 @@ def all(request):
     return render_to_response("pubs.html",{},context_instance = RequestContext(request))
 
 def agregar(request):
-	if request.method == 'POST':
-		form = PublicacionForm(request.POST)
-		if form.is_valid():
-			return HttpResposeRedirect('/pubs')
-	return HttpResposeRedirect('/')
+	publicacion = PublicacionForm(request.POST)
+	publicacion.save()
+	return HttpResposeRedirect('/pubs')
+	# if request.method == 'POST':
+	# 	form = PublicacionForm(request.POST)
+	# 	if form.is_valid():
+	# 		return HttpResposeRedirect('/pubs')
+	# return HttpResposeRedirect('/')
